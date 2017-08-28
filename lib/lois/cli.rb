b6e3d881a5a364eefc3e2cd6c67b7d294787760d
Ryan Hansen <ryan.hansen@octanner.com>
2017-08-27 19:52:03 -0600

    Adding artifact output

--- lib/lois/cli.rb
+++ lib/lois/cli.rb
@@ -4,18 +4,18 @@ module Lois
   class CLI < Thor
     desc 'rubocop', 'Run Rubocop'
     method_option :github_credentials,
-                  aliases: '-g',
-                  required: true,
-                  desc: 'Github credentials to log PR Status.'
+      aliases: '-g',
+      required: true,
+      desc: 'Github credentials to log PR Status.'
     method_option :ci,
-                  default: 'circleci',
-                  aliases: '-c',
-                  desc: 'CI to load env vars from.'
+      default: 'circleci',
+      aliases: '-c',
+      desc: 'CI to load env vars from.'
     def rubocop
       puts 'Checking Rubocop'
       configure(options)
 
-      if system('bundle exec rubocop -f p')
+      if system('bundle exec rubocop -f p -o lois/rubocop.html')
         Lois.config.github.success('rubocop', 'Rubocop passed')
       else
         Lois.config.github.failure('rubocop', 'Rubocop failed')
@@ -24,18 +24,23 @@ module Lois
 
     desc 'bundler-audit', 'Run bundler-audit'
     method_option :github_credentials,
-                  aliases: '-g',
-                  required: true,
-                  desc: 'Github credentials to log PR Status.'
+      aliases: '-g',
+      required: true,
+      desc: 'Github credentials to log PR Status.'
     method_option :ci,
-                  default: 'circleci',
-                  aliases: '-c',
-                  desc: 'CI to load env vars from.'
+      default: 'circleci',
+      aliases: '-c',
+      desc: 'CI to load env vars from.'
     def bundler_audit
       puts 'Checking bundler-audit'
       configure(options)
 
-      if system('bundle exec bundle-audit check --verbose --update')
+      output = `bundle exec bundle-audit check --verbose --update`
+      result = $CHILD_STATUS
+      File.write("lois/bundler-audit.log", output)
+      puts output
+
+      if result.success?
         Lois.config.github.success('bundler-audit', 'No gem vulnerabilities found.')
       else
         Lois.config.github.failure('bundler-audit', 'Gem vulnerabilities detected!')
@@ -44,13 +49,13 @@ module Lois
 
     desc 'reek', 'Run reek'
     method_option :github_credentials,
-                  aliases: '-g',
-                  required: true,
-                  desc: 'Github credentials to log PR Status.'
+      aliases: '-g',
+      required: true,
+      desc: 'Github credentials to log PR Status.'
     method_option :ci,
-                  default: 'circleci',
-                  aliases: '-c',
-                  desc: 'CI to load env vars from.'
+      default: 'circleci',
+      aliases: '-c',
+      desc: 'CI to load env vars from.'
     def reek
       puts 'Checking reek'
       configure(options)
@@ -63,17 +68,17 @@ module Lois
 
     desc 'brakeman', 'Run brakeman'
     method_option :github_credentials,
-                  aliases: '-g',
-                  required: true,
-                  desc: 'Github credentials to log PR Status.'
+      aliases: '-g',
+      required: true,
+      desc: 'Github credentials to log PR Status.'
     method_option :ci,
-                  default: 'circleci',
-                  aliases: '-c',
-                  desc: 'CI to load env vars from.'
+      default: 'circleci',
+      aliases: '-c',
+      desc: 'CI to load env vars from.'
     def brakeman
       puts 'Checking brakeman'
       configure(options)
-      if system('bundle exec brakeman  -o /dev/stdout')
+      if system('bundle exec brakeman -o lois/brakeman.html -o /dev/stdout')
         Lois.config.github.success('brakeman', 'No rails vulnerabilities found.')
       else
         Lois.config.github.failure('brakeman', 'Rails vulnerabilities found.')
@@ -90,6 +95,8 @@ module Lois
         when 'circleci'
           config.ci = Lois::Ci::Circleci.new
         end
+
+        Dir.mkdir('lois') unles Dir.exist?('lois')
 
         config.github = Lois::Github.new(
           config.github_credentials,
